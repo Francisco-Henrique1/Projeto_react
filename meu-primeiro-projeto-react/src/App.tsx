465798f2cb6bf2130596082207943e4db3fd3f06
Francisco Henrique <franciscohenriquebs1@gmail.com>
2026-06-06 22:00:23 -0300

    Primeiro commit do projeto React

---
+++ meu-primeiro-projeto-react/src/App.tsx
@@ -0,0 +1,28 @@
+function App() {
+  return (
+
+    <div>
+
+        <h1>Olá, seja bem vindo!</h1>
+
+        <h2>Me chamo Francisco Henrique</h2>
+
+        <p>Sobre mim</p>
+
+        <h3>
+        Tenho 21 anos, sou de São Pedro do Piaui, atualmente sou academico do<br></br> 
+        Instituito Federal do Piaui Campos Angical do Piaui, estou no 1/ periodo em TADS <br></br>
+        Tecnolóco em Análise e Desenvolvimento de Sistemas.
+        </h3>
+        
+        <p>Hobbies</p>
+
+        <h4>
+          Corrida, caminhada, treino calistenia, músculação, ler livros, assistir filmes e series
+        </h4>
+    </div>
+
+  )
+}
+
+export default App;
